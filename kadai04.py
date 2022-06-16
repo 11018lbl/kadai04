584b7f1625eec04cd893dce0f80cd15b351e5adb
8LBL1101 <kuiqinchuan88@gmail.com>
2022-06-16 19:00:54 +0900

    axisの指定ができないっぽい？ 配列の次元が変っぽいけど出力結果見る限り二次元配列になってそうなんだけど…… あとたぶん各列の最初と最後の数字のダブルクォーテーションがとれてなさそうだから多分strかcharになっちゃってるっていうかこれのせいじゃんね いやまあ真ん中の数字で試してみないことには何とも…… とりあえずよくわかんない

--- kadai04.py
+++ kadai04.py
@@ -1,5 +1,6 @@
+import re
 import pprint
-
+import numpy as np
 
 f=open("FEH_00200521_220610143730.csv",encoding="UTF-8")
 data=f.readlines()
@@ -12,8 +13,17 @@ for t in range(9)  :
         n=str(i)
         num1 = "0"+n+"000"
         if datax.startswith(num1):
-            list.append(datax)
-            print(datax)
+            list1=[]
+            for line in datax.split('","'):
+                line1 = line.strip()
+                if line1:
+                    list1.append(line1)
+                else:
+                    if list1:
+                        #print(",".join(list1))
+                        list1.char()
+            list.append(list1)
+            #print(datax)
     i=i+1
 l=10
 for t in range(9,47)  :
@@ -21,11 +31,31 @@ for t in range(9,47)  :
         n=str(l)
         num1 = n+"000"
         if datax.startswith(num1):
-            list.append(datax)
-            print(datax)
+            list2=[]
+            for line in datax.split('","'):
+                line1 = line.strip()
+                if line1:
+                    list1.append(line1)
+                else:
+                    if list1:
+                        #print(",".join(list1))
+                        list1.char()
+            list.append(list2)
+            #print(datax)
     l=l+1
+#listde=list(filter(None,list))
+np.array(list)
+#np.delete(list,0,0)
+#x = np.mean(list,axis=2)
+#print(x)
+#print("平均値："+np.mean(nplist,axis=2))
+#print("中央値："+np.median(nplist,axis=2))
+#print("最小値："+np.min(nplist,axis=2))
+#print("分散："+np.var(nplist,axis=2))
+#print("標準偏差："+np.std(nplist,axis=2))
 
 print("last")
-pprint.pprint(list,width=50)
+print(list)
+#pprint.pprint(list,width=50)
 f.close()
 print("fin")
