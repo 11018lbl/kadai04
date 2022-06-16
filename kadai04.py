2cebd8a76757398fd26bd20585e4848307f9c433
8LBL1101 <kuiqinchuan88@gmail.com>
2022-06-16 16:06:44 +0900

    抽出したデータをリストに格納

--- kadai04.py
+++ kadai04.py
@@ -1,22 +1,18 @@
-print("hello world")
-f=open("test.txt")
-txt=f.readlines()
-print(txt[1])
-print(txt[0])
-print(txt[2])
-f.close()
+import pprint
+
 
 f=open("FEH_00200521_220610143730.csv",encoding="UTF-8")
 data=f.readlines()
 
 num1 = "0"
-num2 ="0"
 i=1
+list = []
 for t in range(9)  :
     for datax in data:
         n=str(i)
         num1 = "0"+n+"000"
         if datax.startswith(num1):
+            list.append(datax)
             print(datax)
     i=i+1
 l=10
@@ -25,9 +21,11 @@ for t in range(9,47)  :
         n=str(l)
         num1 = n+"000"
         if datax.startswith(num1):
+            list.append(datax)
             print(datax)
     l=l+1
 
 print("last")
+pprint.pprint(list,width=50)
 f.close()
 print("fin")
