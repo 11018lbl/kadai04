d4810ccd387e6f28e77f4de20c72d3aeadef658f
8LBL1101 <kuiqinchuan88@gmail.com>
2022-06-16 23:17:00 +0900

    いったん全部listに格納、cutにsplitで細かく区切って代入してliststrに格納。 liststrに入れたものをfilterできれいにして、キャスト使ってintに変えてってやろうとしたけど栃木-群馬間以降きれなくてもうどうにもわかりませーーーーーーん python簡単って聞いていたけど絶対嘘では？ javaの方が簡単だと思った。個人の感想 謀られたか～～～～～？ なんかforとかif書き方全然違うくてわからん。違うい＃とは。

--- kadai04.py
+++ kadai04.py
@@ -6,15 +6,16 @@ f=open("FEH_00200521_220610143730.csv",encoding="UTF-8")
 data=f.readlines()
 
 num1 = "0"
-i=1
 list = []
-for t in range(9)  :
+p1='[0-9],[a-xA-Z0-9_]|[a-xA-Z0-9],[0-9]|","'
+
+for i in range(1,10)  :
     for datax in data:
         n=str(i)
         num1 = "0"+n+"000"
         if datax.startswith(num1):
             list1=[]
-            for line in datax.split('","'):
+            for line in datax.split(p1):
                 line1 = line.strip()
                 if line1:
                     list1.append(line1)
@@ -23,30 +24,40 @@ for t in range(9)  :
                         #print(",".join(list1))
                         list1.char()
             list.append(list1)
+#            list.append(datax)
             #print(datax)
-    i=i+1
-l=10
-for t in range(9,47)  :
+    #i=i+1
+
+for l in range(9,47)  :
     for datax in data:
         n=str(l)
         num1 = n+"000"
         if datax.startswith(num1):
             list2=[]
-            for line in datax.split('","'):
-                line1 = line.strip()
-                if line1:
-                    list1.append(line1)
+            for line in datax.split(p1):
+                line2 = line.strip()
+                if line2:
+                    list1.append(line2)
                 else:
                     if list1:
                         #print(",".join(list1))
                         list1.char()
             list.append(list2)
+#            list.append(datax)
             #print(datax)
-    l=l+1
+    #l=l+1
+
+liststr=[[]for i in range(46)]
+
+for counter1 in range(46):
+    listdash=str(list[counter1])
+    cut = listdash.split(p1)
+    print(cut)
+
 #listde=list(filter(None,list))
-np.array(list)
+#np.array(list)
 #np.delete(list,0,0)
-#x = np.mean(list,axis=2)
+#x = np.mean(list,axis=)
 #print(x)
 #print("平均値："+np.mean(nplist,axis=2))
 #print("中央値："+np.median(nplist,axis=2))
@@ -56,6 +67,7 @@ np.array(list)
 
 print("last")
 print(list)
+print(list[1][0])
 #pprint.pprint(list,width=50)
 f.close()
 print("fin")
