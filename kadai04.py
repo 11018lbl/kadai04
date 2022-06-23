2e3c2e6985d023c7f709c53ce51760416145b59f
8LBL1101 <kuiqinchuan88@gmail.com>
2022-06-23 19:28:31 +0900

    ファイルを読み込む部分をf=open,f.readlines()からwith open(),csv.readerに変更 リスト化を数字部分に絞るなど 使わなくなったコメントアウト部分の掃除を残して事実上の完成

--- kadai04.py
+++ kadai04.py
@@ -1,10 +1,36 @@
 import re
 import pprint
 import numpy as np
+import csv
+import pandas as pd
 
-f=open("FEH_00200521_220610143730.csv",encoding="UTF-8")
-data=f.readlines()
+#f=open("FEH_00200521_220610143730.csv",encoding="UTF-8")
+#data=f.readlines()
+with open("FEH_00200521_220610143730.csv",'r',encoding="UTF-8") as f:
+    reader=csv.reader(f)
+    n=0
+    list = []
 
+    for row in reader:
+        if n >1:
+            code=row[0]
+            area=row[1]
+            number=re.sub("(,)",'',row[2])
+            if code[2:5] == '000':
+                #print(code,area,number)
+                list.append(int(number))
+
+        n += 1
+
+print("平均値："+str(np.mean(list)))
+print("中央値："+str(np.median(list)))
+print("最小値："+str(np.min(list)))
+print("分散："+str(np.var(list)))
+print("標準偏差："+str(np.std(list)))
+
+f.close()
+
+"""
 num1 = "0"
 list = []
 p1='[0-9],[a-xA-Z0-9_]|[a-xA-Z0-9],[0-9]|","'
@@ -71,3 +97,4 @@ print(list[1][0])
 #pprint.pprint(list,width=50)
 f.close()
 print("fin")
+"""
